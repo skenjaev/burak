59ff13c5a67861354411323d2568c8fad4286c7a
Skenjaev <macbook@macbookui-MacBookPro.local>
2025-06-22 19:35:15 +0900

    BBR: Initial Setup

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+console.log("EXUCUTED");
+
+import moment from "moment";
+
+const currentTime = moment().format("YYYY-MM-DD");
+console.log(currentTime);
+
+const person:string = "Martin";
+const count: number = 100;
