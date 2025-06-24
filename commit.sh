442e6d5fa1652cea43746a83899a6fd7b44e287e
Skenjaev <macbook@macbookui-MacBookPro.local>
2025-06-24 23:41:32 +0900

    feat: [36-37] (Train to'g'rilandi) Environmental variablelar, database va Express & MITASK-H

--- commit.sh
+++ commit.sh
@@ -1,3 +1,3 @@
 git add .
-git commit -m "feat: [36-37] Environmental variablelar, database va Express & MITASK-H"
+git commit -m "feat: [36-37] (Train to'g'rilandi) Environmental variablelar, database va Express & MITASK-H"
 git push origin develop
--- train.ts
+++ train.ts
@@ -1,7 +1,7 @@
 /*H-TASK: shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib, faqat positive qiymatlarni olib string holatda return qilsinMASALAN: getPositive([1, -4, 2]) return qiladi "12"*/
 
 // Faqat musbat sonlarni olib string qaytaruvchi funksiya
-function getPositive(arr) {
+function getPositive(arr: number[]): string { // 'arr' uchun tip va return tipi qo'shildi
     let result = "";  // Bo'sh string yaratamiz
     
     // Array elementlarini birma-bir tekshiramiz
@@ -19,4 +19,4 @@ function getPositive(arr) {
 console.log(getPositive([1, -4, 2]));     
 console.log(getPositive([3, -1, 5, -2])); 
 console.log(getPositive([-1, -2, -3]));   
-console.log(getPositive([7, 8, 9]));      
+console.log(getPositive([7, 8, 9]));
