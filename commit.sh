934965e08734435249b4138a1ebe33896fdefa2c
Skenjaev <macbook@macbookui-MacBookPro.local>
2025-06-24 21:36:42 +0900

    feat: connect mongoDB via mongoose

--- commit.sh
+++ commit.sh
@@ -1,3 +1,3 @@
 git add .
-git commit -m "feat:[35] TypeScript va Patternlar & MITASK-G Oxirgi O'zgarishi"
+git commit -m "feat: connect mongoDB via mongoose"
 git push origin develop
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,15 @@ import dotenv from "dotenv";
 // Faylning to'liq yo'lini ko'rsatish
 dotenv.config({ path: "./src/.env" });
 
-console.log("PORT:", process.env.PORT);
+import mongoose from "mongoose";
 
-console.log("MONGO_URL:", process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URL as string, {})   
+  .then((data) => {
+    console.log("MongoDB connection succeed");
+    const PORT = process.env.PORT ?? 3003;
+  
+  })
+  .catch((err) => {
+    console.log("ERROR on connection MongoDB", err);
+  });
+
