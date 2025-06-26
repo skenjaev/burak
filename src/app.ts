646a16ef6486b4093fb9d2f5883e6162c1628267
Skenjaev <macbook@macbookui-MacBookPro.local>
2025-06-26 18:32:59 +0900

    fix: modify riuting system and controllers

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import router from "./router";
+import routerAdmin from "./routerAdmin";
 
 /** 1-ENTRANCE **/
 const app = express();
@@ -16,7 +17,8 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 /** 4-ROUTERS **/
-app.use("/", router); //middlweare design pattern
+app.use("/admin", routerAdmin);     // EJS
+app.use("/", router);              // REACT //middlweare design pattern
 
 
 export default app;
--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -1,29 +1,7 @@
 import  { Request, Response } from "express";
 import {T} from '../libs/types/common';
 
+// REACT ㅣㅐㅛㅑㅗ므ㅑㅋ ㅕ초ㅕㅜ
 const memberController: T = {};
-memberController.goHome = (req: Request, res: Response) =>{
-    try{
-        res.send("Home Page");
-    }catch (err){
-        console.log("Error, goHome:", err);
-    }
-}; 
-
-memberController.getLogin = (req: Request, res: Response) =>{
-    try{
-        res.send("Login Page");
-    }catch (err){
-        console.log("Error, getLogin:", err);
-    }
-}; 
-
-memberController.getSignup = (req: Request, res: Response) =>{
-    try{
-        res.send("Signup Page");
-    }catch (err){
-        console.log("Error, getSignup:", err);
-    }
-}; 
 
 export default memberController;
