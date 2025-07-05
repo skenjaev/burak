cade984635907d17f1f7365b2237bbae7950b2b1
Skenjaev <macbook@macbookui-MacBookPro.local>
2025-07-06 05:07:57 +0900

    feat:frontend related css & js and image creation

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -12,7 +12,7 @@ const restaurantController: T = {};
 restaurantController.goHome = (req: Request, res: Response) => {
     try {
         console.log("goHome");
-        res.send("Home Page");
+        res.render("home");
     } catch (err) {
         console.log("Error, goHome:", err);
         res.send(err); // Xatolikni qaytarish
@@ -22,7 +22,7 @@ restaurantController.goHome = (req: Request, res: Response) => {
 restaurantController.getSignup = (req: Request, res: Response) => {
     try {
         console.log("getSignup");
-        res.send("Signup Page");
+        res.render("Signup");
     } catch (err) {
         console.log("Error, getSignup:", err);
         res.send(err); // Xatolikni qaytarish
@@ -32,7 +32,7 @@ restaurantController.getSignup = (req: Request, res: Response) => {
 restaurantController.getLogin = (req: Request, res: Response) => {
     try {
         console.log("getLogin");
-        res.send("Login Page");
+        res.render("login");
     } catch (err) {
         console.log("Error, getLogin:", err);
         res.send(err); // Xatolikni qaytarish
