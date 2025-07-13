8ea3c680ab20f11f677a66f4e129a51518916bba
Skenjaev <macbook@macbookui-MacBookPro.local>
2025-07-13 18:44:26 +0900

    feat: updateChosenProduct business logic

--- src/libs/config.ts
+++ src/libs/config.ts
@@ -1,1 +1,7 @@
-export const MORGAN_FORMAT = `:method :url :response-time [:status] \n`; //// MORGAN_FORMAT ni import qilish // Node.js uchun HTTP so'rovlarini yozib boruvchi middleware:
+export const MORGAN_FORMAT = `:method :url  :response-time [:status] \n`;
+
+import mongoose from "mongoose";
+
+export const shapeIntoMongooseObjectId = (target: any) => {
+    return typeof target === "string" ? new mongoose.Types.ObjectId(target) : target;
+}
