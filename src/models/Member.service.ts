b2e4d09bc6d746808f44fff937541f0a2c1b7ecf
Skenjaev <macbook@macbookui-MacBookPro.local>
2025-08-10 18:26:06 +0900

    feat: create getRestaurant rest API

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -24,6 +24,16 @@ class MemberService {
   }
 
   /*  SPA  */
+
+  public async getRestaurant(): Promise<Member> {
+    const result = await this.memberModel
+     .findOne({ memberType: MemberType.RESTAURANT})
+      .exec();
+      if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+
+    return result
+    }
+
   public async signup(input: MemberInput): Promise<Member> {
     const salt = await bcrypt.genSalt();
     input.memberPassword = await bcrypt.hash(input.memberPassword, salt);
@@ -106,8 +116,9 @@ class MemberService {
         memberPoints: { $gte: 1 },
       })
       .sort({ memberPoints: -1 })
-      .limit(4).exec();
-      if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+      .limit(4)
+      .exec();
+    if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
 
     return result;
   }
